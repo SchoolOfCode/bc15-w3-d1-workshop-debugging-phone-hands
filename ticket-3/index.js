5f2fc27a6df115dd8b38256d00d1825b983a76cf
Christopher Ndibe <139502506+cndibe1107@users.noreply.github.com>
2023-09-18 16:17:11 +0100

    ticket 3

--- ticket-3/index.js
+++ ticket-3/index.js
@@ -3,11 +3,11 @@ function countNumberOfNinesInNumber(number) {
 
   for (let i = 0; i < number.length; i++) {
     const digit = number[i];
-    if (9 === digit) {
+    if ("9" === digit) {
       count++;
     }
   }
-   
   return count;
 }
-alert(countNumberOfNinesInNumber(9));
+alert(countNumberOfNinesInNumber("99.999"));
+console.log(countNumberOfNinesInNumber("99.999"))
